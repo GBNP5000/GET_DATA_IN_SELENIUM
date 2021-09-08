3d0b94eaa0d3fcbad1286e3e55319746b23fc5f9
GBNP5000 <41781833+GBNP5000@users.noreply.github.com>
2021-09-08 22:44:02 +0530

    Update GET-NSE-SELE.py

--- GET-NSE-SELE.py
+++ GET-NSE-SELE.py
@@ -2,6 +2,10 @@
     SAMPLE CODE TAKEN FROM
     
     https://thinkdiff.net/how-to-scrap-data-from-javascript-based-website-using-python-selenium-and-headless-web-driver-531c7fe0c01f
+    
+    WANT TO GET DATA FROM
+    
+       https://www.nseindia.com/get-quotes/equity?symbol=PSUBNKBEES
 """
 
 from selenium import webdriver
@@ -18,13 +22,15 @@ chrome_options = Options()
 driver = webdriver.Chrome(
     executable_path=chrome_driver_path, options=chrome_options
 )
-
+# with webdriver as driver:
 driver.get('https://www.nseindia.com/get-quotes/equity?symbol=PSUBNKBEES')
+# driver.get('https://quotes.toscrape.com/js/')
 
-wait = WebDriverWait(driver, 20)
+wait = WebDriverWait(driver, 200)
 
 wait.until(itmloc((By.ID, "#quoteLtp")))
 # sleep(10)
-PRI = driver.find_element_by_id('#quoteLtp')
-driver.close()
+PRI = driver.find_element_by_id(By.ID, '#quoteLtp')
+# driver.close()
 print(PRI)
+
